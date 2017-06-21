f12d79e59e817ad90ed78c199c9ed55b50158551
Yannick Dalbin <yannick@idvive.com>
2017-06-21 23:14:42 +0200

    TP3.6: Dans ce même fichier, ajoutez une action user qui va permettre d'ajouter un utilisateur

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,5 +3,10 @@ const users = ["Toto", "Tata"]
 module.exports = {
   usersAction: function (req, res) {
     res.json(users)
+  },
+  userAction: function (req, res) {
+    const user = req.body.user
+    users.push(user)
+    res.end()
   }
 }
