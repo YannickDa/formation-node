b73ba93f79335a1516ead8990e159905e2a31918
Yannick Dalbin <yannick@idvive.com>
2017-06-21 23:15:04 +0200

    TP3.7: Montez les actions sur les routes /users en méthode GET et /user en méthode POST

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const IndexController = require("./controllers/IndexController")
+const UsersController = require("./controllers/UsersController")
 
 const server = express()
 
+server.use(bodyParser.json())
+
 server.get("/", IndexController.indexAction)
+
+server.get("/users", UsersController.usersAction)
+server.post("/user", UsersController.userAction)
 
 server.listen(3000)
